edace4cebd19e4591f394d2c4dbb4cc014fe40c6
Ana Galvez <anamariagalvez01@gmail.com>
2025-03-10 01:03:55 -0300

    feat: expresiones lambda 3

--- ExpresionesLambdasYExpresionesRegulares/Program.cs
+++ ExpresionesLambdasYExpresionesRegulares/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 namespace ExpresionesLambdasYExpresionesRegulares
@@ -15,18 +16,25 @@ namespace ExpresionesLambdasYExpresionesRegulares
             persona2.Nombre = "juan";
             persona2.Edad = 38;
 
-            OperacionesMatematicasUnNumero cuadrado = new OperacionesMatematicasUnNumero(numero => numero * numero);
-            OperacionesMatematicasDosNumeros suma = new OperacionesMatematicasDosNumeros((numero1,numero2) => numero1 + numero2);
-            Console.WriteLine(cuadrado(9));
-            Console.WriteLine(suma(15,35));
+            //OperacionesMatematicasUnNumero cuadrado = new OperacionesMatematicasUnNumero(numero => numero * numero);
+            //OperacionesMatematicasDosNumeros suma = new OperacionesMatematicasDosNumeros((numero1,numero2) => numero1 + numero2);
+            //Console.WriteLine(cuadrado(9));
+            //Console.WriteLine(suma(15,35));
+
+            List<int> numeros = new List<int>() {1,2,3,4,5,6,7,8,9,10};
+            List<int> numerosPares = numeros.FindAll(numero => numero % 2 == 0);
+            List<int> numerosImpares = numeros.FindAll(numero => numero % 2 != 0);
+            foreach (int item in numerosPares) Console.WriteLine(item);
+            numerosImpares.ForEach(numero => Console.WriteLine(numero));
+
         }
 
-        delegate int OperacionesMatematicasUnNumero(int numero);
-        delegate int OperacionesMatematicasDosNumeros(int numero1, int numero2);
-        //static int Cuadrado(int numero)
-        //{
-        //    return numero * numero;
-        //}
+        //delegate int OperacionesMatematicasUnNumero(int numero);
+        //delegate int OperacionesMatematicasDosNumeros(int numero1, int numero2);
+        ////static int Cuadrado(int numero)
+        ////{
+        ////    return numero * numero;
+        ////}
     }
 
     class Personas
